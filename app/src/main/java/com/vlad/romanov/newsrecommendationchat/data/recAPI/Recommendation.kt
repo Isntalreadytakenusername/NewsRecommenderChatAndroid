b0e3dda66570ef3688fdf71ce6d766881d5c2702
Vladyslav Romanov <vlad.vik.romanov@gmail.com>
2024-04-06 19:20:42 +0200

    put all the api stuff into one file

--- app/src/main/java/com/vlad/romanov/newsrecommendationchat/data/recAPI/Recommendation.kt
+++ app/src/main/java/com/vlad/romanov/newsrecommendationchat/data/recAPI/Recommendation.kt
@@ -1,7 +1,27 @@
 package com.vlad.romanov.newsrecommendationchat.data.recAPI
 
+import com.google.gson.GsonBuilder
 import com.google.gson.annotations.SerializedName
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 data class Recommendation(
     @SerializedName("Hello") val testingString: String
 )
+
+class RecommendationInstance {
+    companion object {
+        val recommendationService = Retrofit.Builder()
+            .baseUrl("http://51.20.85.27")
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+            .create(RecommendationService::class.java)
+    }
+}
+
+interface RecommendationService {
+    @GET("/")
+    suspend fun getRecommendation(): Response<Recommendation>
+}
--- app/src/main/java/com/vlad/romanov/newsrecommendationchat/data/recAPI/RecommendationService.kt
+++
@@ -1,9 +0,0 @@
-package com.vlad.romanov.newsrecommendationchat.data.recAPI
-
-import retrofit2.Response
-import retrofit2.http.GET
-
-interface RecommendationService {
-    @GET("/")
-    suspend fun getRecommendation(): Response<Recommendation>
-}
